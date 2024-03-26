696e3446330a1e25d220ebd146a50883eded8cff
Uros Terzic <urosterzicbiz@gmail.com>
2024-03-26 20:05:59 +0100

    Can find appropriate promotions based on request date

---
+++ src/DTO/LowestPriceEnquiry.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\DTO;
+
+use App\Entity\Product;
+
+class LowestPriceEnquiry implements PromotionEnquiryInterface
+{
+    private ?Product $product;
+
+    public function getProduct(): ?Product
+    {
+        return $this->product;
+    }
+
+    public function setProduct(?Product $product): void
+    {
+        $this->product = $product;
+    }
+
+    public function getQuantity(): ?int
+    {
+        return $this->quantity;
+    }
+
+    public function setQuantity(?int $quantity): void
+    {
+        $this->quantity = $quantity;
+    }
+
+    public function getRequestLocation(): ?string
+    {
+        return $this->requestLocation;
+    }
+
+    public function setRequestLocation(?string $requestLocation): void
+    {
+        $this->requestLocation = $requestLocation;
+    }
+
+    public function getVoucherCode(): ?string
+    {
+        return $this->voucherCode;
+    }
+
+    public function setVoucherCode(?string $voucherCode): void
+    {
+        $this->voucherCode = $voucherCode;
+    }
+
+    public function getRequestDate(): ?string
+    {
+        return $this->requestDate;
+    }
+
+    public function setRequestDate(?string $requestDate): void
+    {
+        $this->requestDate = $requestDate;
+    }
+
+    public function getPrice(): ?int
+    {
+        return $this->price;
+    }
+
+    public function setPrice(?int $price): void
+    {
+        $this->price = $price;
+    }
+
+    public function getDiscountedPrice(): ?int
+    {
+        return $this->discountedPrice;
+    }
+
+    public function setDiscountedPrice(?int $discountedPrice): void
+    {
+        $this->discountedPrice = $discountedPrice;
+    }
+
+    public function getPromotionId(): ?int
+    {
+        return $this->promotionId;
+    }
+
+    public function setPromotionId(?int $promotionId): void
+    {
+        $this->promotionId = $promotionId;
+    }
+
+    public function getPromotionName(): ?string
+    {
+        return $this->promotionName;
+    }
+
+    public function setPromotionName(?string $promotionName): void
+    {
+        $this->promotionName = $promotionName;
+    }
+    private ?int $quantity;
+    private ?string $requestLocation;
+    private ?string $voucherCode;
+    private ?string $requestDate;
+    private ?int $price;
+    private ?int $discountedPrice;
+    private ?int $promotionId;
+    private ?string $promotionName;
+
+    public function jsonSerialize(): array
+    {
+        return get_object_vars($this);
+    }
+}
